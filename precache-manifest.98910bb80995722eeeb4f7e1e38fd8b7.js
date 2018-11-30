dffb834cc575cbd97e14b587a1231abae637feb6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 06:55:54 +0000

    Deploy craftybones/gol to github.com/craftybones/gol.git:gh-pages

---
+++ precache-manifest.98910bb80995722eeeb4f7e1e38fd8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d26652ec4afd38cabc3a",
+    "url": "/gol/static/js/runtime~main.d26652ec.js"
+  },
+  {
+    "revision": "e7ca65aaee301355b736",
+    "url": "/gol/static/js/main.e7ca65aa.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/gol/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "e7ca65aaee301355b736",
+    "url": "/gol/static/css/main.5b6f6ca5.chunk.css"
+  },
+  {
+    "revision": "5c7ea54ecd56512d32c1fa3ca35f45e7",
+    "url": "/gol/index.html"
+  }
+];
